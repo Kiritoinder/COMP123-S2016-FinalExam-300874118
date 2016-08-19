ab243adbdde8c2be50765531bb27daebaa4764cc
Kiritoinder <inderbharaj@ymail.com>
2016-08-19 11:05:40 -0400

    Modified Character class

--- COMP123-S2016-FinalExam-300874118/Character.cs
+++ COMP123-S2016-FinalExam-300874118/Character.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+
+//Class modified by Inderjeet Singh
+// Student I.D- 300874118
+// Date Modified: August 19,2016
 namespace COMP123_S2016_FinalExam_300874118
 {
     public class Character
@@ -20,5 +24,11 @@ namespace COMP123_S2016_FinalExam_300874118
         public string Charisma { get; set; }
 
         public string Race { get; set; }
+
+        // Added FirstName property
+        public string FirstName { get; set; }
+
+        //Added LastName Property
+        public string LastName { get; set; }
     }
 }
